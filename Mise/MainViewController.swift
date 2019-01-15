345e3a5033058013744aec379fe5a7cd6aac7a15
Kang Seongchan <zelatool@gmail.com>
2019-01-15 21:54:32 +0900

    [F]ScrollView

--- Mise/MainViewController.swift
+++ Mise/MainViewController.swift
@@ -25,6 +25,7 @@ class MainViewController: UIViewController {
     let infoStackView2 = UIStackView()
     let alertLb = UILabel()
     let tapGesture = UITapGestureRecognizer()
+    let emptyView = UIView()
 
     var locationManager:CLLocationManager!
     var canUpdated:Bool {
@@ -51,10 +52,11 @@ class MainViewController: UIViewController {
 
         bannerView = GADBannerView(adSize: kGADAdSizeBanner)
 
-        baseScrollView.addSubview([thumImageView, locationLb, dustLb, infoStackView, infoStackView2, alertLb])
-        view.addSubview([baseScrollView, bannerView])
+        baseScrollView.contentView.addSubview([thumImageView, locationLb, dustLb, infoStackView, infoStackView2, alertLb, emptyView, bannerView])
+        view.addSubview([baseScrollView])
+        baseScrollView.setScrollView(vc: self)
 
-        baseScrollView.snp.makeConstraints { (make) in
+        baseScrollView.snp.remakeConstraints { (make) in
             make.top.leading.trailing.equalToSuperview()
             make.bottom.equalTo(-50)
         }
@@ -65,15 +67,17 @@ class MainViewController: UIViewController {
         thumImageView.snp.makeConstraints { (make) in
 //            make.center.equalToSuperview()
             make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview().offset(-20)
+            make.top.equalTo(150)
+//            make.top.equalTo(view.safeArea.top).offset(150)
             make.width.equalToSuperview().multipliedBy(0.7)
             
-            make.height.equalTo(250)
+            make.height.equalTo(300)
         }
         
         locationLb.snp.makeConstraints { (make) in
             make.bottom.equalTo(thumImageView.snp.top).offset(-20)
             make.leading.equalTo(10)
+            make.height.equalTo(40)
             make.centerX.equalToSuperview()
         }
         
@@ -88,20 +92,32 @@ class MainViewController: UIViewController {
             make.centerX.equalToSuperview()
             make.top.equalTo(thumImageView.snp.bottom).offset(20)
             make.leading.equalTo(10)
+            make.height.equalTo(20)
         }
         
         infoStackView.snp.makeConstraints { (make) in
             make.top.equalTo(dustLb.snp.bottom).offset(10)
             make.leading.equalTo(10)
+            make.height.equalTo(100)
             make.centerX.equalToSuperview()
         }
         
         infoStackView2.snp.makeConstraints { (make) in
             make.top.equalTo(infoStackView.snp.bottom).offset(10)
             make.leading.equalTo(10)
+            make.height.equalTo(100)
             make.centerX.equalToSuperview()
-            make.bottom.equalTo(-50)
+//            make.bottom.equalToSuperview()
+//            make.bottom.equalTo(-50)
         }
+        
+        emptyView.snp.makeConstraints { (make) in
+            make.height.equalTo(400)
+            make.top.equalTo(infoStackView2.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        emptyView.backgroundColor = .red
         infoStackView.axis = .horizontal
         infoStackView.distribution = .fillEqually
         infoStackView2.distribution = .fillEqually
@@ -164,16 +180,16 @@ class MainViewController: UIViewController {
 
         }
         
-        let v1 = InfoView.init(title: "Humidity", value: "\(weatherData.humidity!)") // 습도
-        let v2 = InfoView.init(title: "O3", value: "\(weatherData.o3!)") // 오존
-        let v3 = InfoView.init(title: "SO2", value: "\(weatherData.so2!)") // 이산화황
-        let v4 = InfoView.init(title: "PM2.5", value: "\(weatherData.pm25!)") // 초미세먼지
-        let v5 = InfoView.init(title: "PM10", value: "\(weatherData.pm10!)") // 미세먼지
-        let v6 = InfoView.init(title: "Pressure", value: "\(weatherData.pressure!)") // 기압
-        let v7 = InfoView.init(title: "Wind", value: "\(weatherData.wind!)") // 풍향
-        let v8 = InfoView.init(title: "CO", value: "\(weatherData.co!)") // 일산화탄소
-        let v9 = InfoView.init(title: "Temp", value: "\(weatherData.temperature!)") // 기온
-        let v10 = InfoView.init(title: "rain", value: "\(weatherData.rain!)") // 강수확률
+        let v1 = InfoView.init(title: "Humidity", value: "\(weatherData.humidity ?? 0)") // 습도
+        let v2 = InfoView.init(title: "O3", value: "\(weatherData.o3 ?? 0)") // 오존
+        let v3 = InfoView.init(title: "SO2", value: "\(weatherData.so2 ?? 0)") // 이산화황
+        let v4 = InfoView.init(title: "PM2.5", value: "\(weatherData.pm25 ?? 0)") // 초미세먼지
+        let v5 = InfoView.init(title: "PM10", value: "\(weatherData.pm10 ?? 0)") // 미세먼지
+        let v6 = InfoView.init(title: "Pressure", value: "\(weatherData.pressure ?? 0)") // 기압
+        let v7 = InfoView.init(title: "Wind", value: "\(weatherData.wind ?? 0)") // 풍향
+        let v8 = InfoView.init(title: "CO", value: "\(weatherData.co ?? 0)") // 일산화탄소
+        let v9 = InfoView.init(title: "Temp", value: "\(weatherData.temperature ?? 0)") // 기온
+        let v10 = InfoView.init(title: "rain", value: "\(weatherData.rain ?? 0)") // 강수확률
 
         
         _ = [v1, v2, v3, v4, v5].map({infoStackView.addArrangedSubview($0)})
@@ -183,39 +199,43 @@ class MainViewController: UIViewController {
     
     func getInfo() {
         
-//        if let myLocation = locationManager.location {
-//
-//            CLGeocoder().reverseGeocodeLocation(myLocation) { (places, error) in
-//
-//                if let place = places?[0] {
-//
-//                    CustomAPI.getDust(lat:"\(myLocation.coordinate.latitude)", lng: "\(myLocation.coordinate.longitude)", completion: { (weather) in
-//                        print(weather)
-//
-//                        self.setData(weather, locationName: place.locality ?? weather.name)
-//
-//                    })
-//                }
-//            }
-        
-            if let myLocation = locationManager.location {
-                
-                UserDefaults.standard.set(["en"], forKey: "AppleLanguages")
+        if let myLocation = locationManager.location {
 
-                CLGeocoder().reverseGeocodeLocation(myLocation) { (places, error) in
-                    
-                    if let place = places?[0] {
-                        
-                        CustomAPI.getDust(city: place.administrativeArea!, completion: { (weather) in
-                            print(weather)
-                            
-                            self.setData(weather, locationName: weather.name)
-                            UserDefaults.standard.removeObject(forKey: "AppleLanguages")
+            CLGeocoder().reverseGeocodeLocation(myLocation) { (places, error) in
 
-                        })
-                    }
+                if let place = places?[0] {
+
+                    CustomAPI.getDust(lat:"\(myLocation.coordinate.latitude)", lng: "\(myLocation.coordinate.longitude)", completion: { (weather) in
+                        print(weather)
+
+                        self.setData(weather, locationName: place.locality ?? weather.name)
+
+                    })
                 }
             }
+        }
+//            if let myLocation = locationManager.location {
+//
+//                UserDefaults.standard.set(["en"], forKey: "AppleLanguages")
+//
+//                print(myLocation)
+//
+//                CLGeocoder().reverseGeocodeLocation(myLocation) { (places, error) in
+//
+//                    if let place = places?[0] {
+//                        print(place)
+//                        print("~~")
+//
+//                        CustomAPI.getDust(city: place.administrativeArea!, completion: { (weather) in
+//                            print(weather)
+//
+//                            self.setData(weather, locationName: weather.name)
+//                            UserDefaults.standard.removeObject(forKey: "AppleLanguages")
+//
+//                        })
+//                    }
+//                }
+//            }
     }
 }
 
@@ -245,6 +265,9 @@ extension MainViewController:CLLocationManagerDelegate {
             print(" case .restricted:")
 
         case .notDetermined:
+//            locationManager.requestLocation()
+            locationManager.requestAlwaysAuthorization()
+
             print(" case .notDetermined:")
             
         }
