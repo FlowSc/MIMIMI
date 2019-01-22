eeed5840fcac71cbf2896398baca164fb676db50
seongchan <seongchan@edkim.co>
2019-01-22 18:21:56 +0900

    [F]String

--- MiseWidget/TodayViewController.swift
+++ MiseWidget/TodayViewController.swift
@@ -75,42 +75,42 @@ class TodayViewController: UIViewController, NCWidgetProviding {
                 case .bad:
                     self.view.backgroundColor = UIColor.unhealthyRed
                     
-                    let attrS = "\(data.time)\n\(data.aqi)㎍/m³\n".makeAttrString(font: .NotoSans(.bold, size: 14), color: .white)
+                    let attrS = "\(data.time)\n\(data.aqi)\n".makeAttrString(font: .NotoSans(.bold, size: 14), color: .white)
                     attrS.append("badDesc".localized.makeAttrString(font: .NotoSans(.bold, size: 14), color: .white))
                     
                     self.widgetLb.attributedText = attrS
 //                    self.widgetLb.attributedText.add
 
                 case .danger:
-                    let attrS = "\(data.time)\n\(data.aqi)㎍/m³\n".makeAttrString(font: .NotoSans(.bold, size: 14), color: .white)
+                    let attrS = "\(data.time)\n\(data.aqi)\n".makeAttrString(font: .NotoSans(.bold, size: 14), color: .white)
                     attrS.append("dangerDesc".localized.makeAttrString(font: .NotoSans(.bold, size: 14), color: .white))
                     
                     self.widgetLb.attributedText = attrS
                     self.view.backgroundColor = UIColor.hazardPurple
                     
                 case .little:
-                    let attrS = "\(data.time)\n\(data.aqi)㎍/m³\n".makeAttrString(font: .NotoSans(.bold, size: 14), color: .white)
+                    let attrS = "\(data.time)\n\(data.aqi)\n".makeAttrString(font: .NotoSans(.bold, size: 14), color: .white)
                     attrS.append("littleBadDesc".localized.makeAttrString(font: .NotoSans(.bold, size: 14), color: .white))
                     
                     self.widgetLb.attributedText = attrS
                     self.view.backgroundColor = UIColor.unhealthyTangerine
                     
                 case .normal:
-                    let attrS = "\(data.time)\n\(data.aqi)㎍/m³\n".makeAttrString(font: .NotoSans(.bold, size: 14), color: .white)
+                    let attrS = "\(data.time)\n\(data.aqi)\n".makeAttrString(font: .NotoSans(.bold, size: 14), color: .white)
                     attrS.append("normalDesc".localized.makeAttrString(font: .NotoSans(.bold, size: 14), color: .white))
                     
                     self.widgetLb.attributedText = attrS
                     self.view.backgroundColor = UIColor.normalYellow
                     
                 case .safe:
-                    let attrS = "\(data.time)\n\(data.aqi)㎍/m³\n".makeAttrString(font: .NotoSans(.bold, size: 14), color: .white)
+                    let attrS = "\(data.time)\n\(data.aqi)\n".makeAttrString(font: .NotoSans(.bold, size: 14), color: .white)
                     attrS.append("safeDesc".localized.makeAttrString(font: .NotoSans(.bold, size: 14), color: .white))
                     
                     self.widgetLb.attributedText = attrS
                     self.view.backgroundColor = UIColor.wellGreen
                     
                 case .veryBad:
-                    let attrS = "\(data.time)\n\(data.aqi)㎍/m³\n".makeAttrString(font: .NotoSans(.bold, size: 14), color: .white)
+                    let attrS = "\(data.time)\n\(data.aqi)\n".makeAttrString(font: .NotoSans(.bold, size: 14), color: .white)
                     attrS.append("veryBadDesc".localized.makeAttrString(font: .NotoSans(.bold, size: 14), color: .white))
                     
                     self.widgetLb.attributedText = attrS
